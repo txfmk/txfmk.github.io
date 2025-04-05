0e028d0b8dd57925da50c7e70e87b890a3be708f
“txfmk” <3524016578@qq.com>
2025-04-05 14:11:12 +0800

    Site updated: 2025-04-05 14:11:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/hello (copy)/","2025/04/04/hello/","2025/04/02/hello-world/"];function toRandomPost(){
+var posts=["2025/04/04/hello (copy)/","2025/04/02/hello-world/","2025/04/04/hello/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
