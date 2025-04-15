d4ed316190b3c8346a6d44ccbd199a35ff88f51a
“txfmk” <3524016578@qq.com>
2025-04-15 13:07:18 +0800

    Site updated: 2025-04-15 13:07:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/07/ctfshow web(php特性wp)/","2025/04/07/php命令执行/","2025/04/07/php反序列化/","2025/04/07/搭blog/"];function toRandomPost(){
+var posts=["2025/04/07/ctfshow web(php特性wp)/","2025/04/07/php命令执行/","2025/04/07/搭blog/","2025/04/07/php反序列化/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
