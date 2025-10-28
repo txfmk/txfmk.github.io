60c6f2abb7d61ab9440feb5b72e8f729d02b1d96
txfmk <3524016578@qq.com>
2025-10-28 09:45:20 +0800

    Site updated: 2025-10-28 09:45:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/07/ctfshow web(php特性wp)/","2025/04/07/php反序列化/","2025/04/29/ctfshow_web-ssti/","2025/05/21/轩辕杯wp/","2025/04/07/php命令执行/","2025/04/07/搭blog/"];function toRandomPost(){
+var posts=["2025/04/07/ctfshow web(php特性wp)/","2025/04/07/php反序列化/","2025/04/07/php命令执行/","2025/04/29/ctfshow_web-ssti/","2025/04/07/搭blog/","2025/05/21/轩辕杯wp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
